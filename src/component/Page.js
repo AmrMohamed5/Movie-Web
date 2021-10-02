b072e45aed3ded3c5e8c47521a52d50d74539805
Amr Mohamed <amr.mohamed5053@outlook.com>
2021-10-02 21:21:30 +0200

    Adding more feature and Styling page component

--- src/component/Page.js
+++ src/component/Page.js
@@ -26,50 +26,119 @@ function Page({ id }) {
       .then((err) => setError(err));
     setLoading(true);
   }
-  console.log(movie.production_countries);
-
+  console.log(movie);
+  const prod = movie.production_companies;
+  const gener = movie.genres;
+  const country = movie.production_countries;
   return (
     <div className="container">
       <div className="page">
-        <div className="card mb-3" style={{ maxWidth: "640px" }}>
-          <div className="row g-0">
-            <div className="col-md-4">
-              <img
-                src={`https://image.tmdb.org/t/p/w1280${movie.poster_path}`}
-                className="img-fluid rounded-start"
-                alt="movie"
-              />
-            </div>
-            <div className="col-md-8">
-              <div className="card-body">
-                <h2 className="card-title">{movie.title}</h2>
-                <h5>{movie.tagline} </h5>
-                <p className="card-text">{movie.overview}</p>
-                <div className="card-text">
-                  <h6> {movie.vote_average} </h6>
-                  <small className="text-muted">{movie.release_date}</small>
-                </div>
+        <div className="page-head" style={{ margin: "10px 0" }}>
+          <div className="container">
+            <div className="row">
+              <div className="col-lg-4">
+                <img
+                  src={`https://image.tmdb.org/t/p/w1280${movie.poster_path}`}
+                  className="img-thumbnail rounded float-start"
+                  alt="movie"
+                />
+              </div>
+              <div className="col-lg-8">
+                <table className="table table-hover table-bordered">
+                  <thead>
+                    <tr>
+                      <th style={{ textAlign: "center" }} colSpan="2">
+                        {movie.title}
+                      </th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td
+                        style={{ textAlign: "center" }}
+                        colSpan="2"
+                        scope="row"
+                      >
+                        {movie.tagline}
+                      </td>
+                    </tr>
+                    <tr>
+                      <th scope="row">Language</th>
+                      <td>{movie.original_language}</td>
+                    </tr>
+                    <tr>
+                      <th scope="row">Countries</th>
+                      <td>
+                        {country &&
+                          country.map((e) => (
+                            <span style={{ margin: "5px" }} key={Math.random()}>
+                              {e.name}
+                            </span>
+                          ))}
+                      </td>
+                    </tr>
+                    <tr>
+                      <th scope="row">Type</th>
+                      <td>
+                        {gener &&
+                          gener.map((e) => (
+                            <span key={Math.random()} style={{ margin: "5px" }}>
+                              {e.name}
+                            </span>
+                          ))}
+                      </td>
+                    </tr>
+                    <tr>
+                      <th scope="row">Rate</th>
+                      <td>{movie.vote_average}</td>
+                    </tr>
+                    <tr>
+                      <th scope="row">Date</th>
+                      <td>{movie.release_date}</td>
+                    </tr>
+                  </tbody>
+                </table>
               </div>
             </div>
           </div>
         </div>
         <hr />
-        <div className="producers">
-          <ul>
-            <li></li>
-            <li></li>
-            <li></li>
-          </ul>
+        <div className="related">
+          <h6>People who watches this movie {movie.vote_count},000</h6>
+        </div>
+        <hr />
+        <div className="overview">
+          <h4> Story </h4>
+          <p> {movie.overview} </p>
+        </div>
+        <hr />
+        <div
+          className="producers"
+          style={{ display: "flex", justifyContent: "center" }}
+        >
+          {prod &&
+            prod.map((e) =>
+              e.logo_path != null ? (
+                <img
+                  key={Math.random()}
+                  style={{ width: "100px", height: "auto", margin: "auto" }}
+                  src={`https://image.tmdb.org/t/p/w1280${e.logo_path}`}
+                  alt="production"
+                />
+              ) : (
+                <span style={{ display: "none" }}></span>
+              )
+            )}
         </div>
         <hr />
         {loading == true ? (
-          <div className="trailer">
+          <div className="trailer" style={{ marginTop: "30px" }}>
             {movieVideos.results != null && movieVideos.results.length != 0 ? (
               <div>
                 <h2> {movieVideos.results[0].name} </h2>
                 <iframe
-                  width="700"
-                  height="400"
+                  width="1200"
+                  height="500"
                   src={
                     "https://www.youtube.com/embed/" +
                     movieVideos.results[0].key
